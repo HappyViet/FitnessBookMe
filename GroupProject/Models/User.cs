42a663880498e6de8ecbaaedd46ed142c6d8b8e4
pjoshi91 <pranavjoshi@csu.fullerton.edu>
2017-09-25 15:02:45 -0700

    Added authentication for Admin and also created Admin Dashboard and related page's layouts, added EF Core with MySQL

---
+++ GroupProject/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class User
+    {
+        public long UserID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public int UserRole { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        public string GetRole()
+        {
+            if (UserRole >= 50)
+                return "Administrator";
+            if (UserRole == 49)
+                return "Instructor";
+            return "";
+        }
+    }
+}
--- GroupProject/Startup.cs
+++ GroupProject/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using GroupProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace GroupProject
 {
@@ -21,7 +25,46 @@ namespace GroupProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("DB_Connection");
+            services.AddSingleton<DB_Context>();
+            services.AddAuthentication("Fitness247UserAuthentication")
+                .AddCookie("Fitness247UserAuthentication", options =>
+                {
+                    options.AccessDeniedPath = new PathString("/");
+                    options.Cookie = new CookieBuilder
+                    {
+                        //Domain = "",
+                        HttpOnly = true,
+                        Name = ".Fitness.24x7.Cookie",
+                        Path = new PathString("/"),
+                        SameSite = SameSiteMode.Lax,
+                        SecurePolicy = CookieSecurePolicy.SameAsRequest
+                    };
+                    options.LoginPath = new PathString("/login");
+                    options.ReturnUrlParameter = "RequestPath";
+                    options.SlidingExpiration = true;
+                })
+                .AddCookie("Fitness247AdminAuthentication", options =>
+                 {
+                     options.AccessDeniedPath = new PathString("/admin");
+                     options.Cookie = new CookieBuilder
+                     {
+                         //Domain = "",
+                         HttpOnly = true,
+                         Name = ".Fitness.24x7.Admin.Cookie",
+                         Path = new PathString("/admin"),
+                         SameSite = SameSiteMode.Lax,
+                         SecurePolicy = CookieSecurePolicy.SameAsRequest
+                     };
+                     options.LoginPath = new PathString("/admin/login");
+                     options.ReturnUrlParameter = "RequestPath";
+                     options.SlidingExpiration = true;
+                 });
+
+            
+            services.AddRouting(options => options.LowercaseUrls = true);
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +78,7 @@ namespace GroupProject
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            app.UseAuthentication();
 
             app.UseStaticFiles();
 
