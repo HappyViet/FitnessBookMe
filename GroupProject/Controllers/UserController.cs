aed8a40a8aaa23a5bb744f3329a08ef561d9589d
pjoshi91 <pranavjoshi@csu.fullerton.edu>
2017-11-06 17:07:45 -0800

    Updates

---
+++ GroupProject/Controllers/UserController.cs
@@ -0,0 +1,130 @@
+﻿using GroupProject.Constants;
+using GroupProject.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GroupProject.Controllers
+{
+    [Authorize(AuthenticationSchemes = "Fitness247UserAuthentication")]
+    public class UserController:Controller
+    {
+        private DB_Context db_context;
+
+        public UserController(DB_Context db_context)
+        {
+            this.db_context = db_context;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            User loggedInuser = db_context.Users.Where(x => x.Email == HttpContext.User.Identity.Name || x.UserName == HttpContext.User.Identity.Name).ToList().First();
+            ViewBag.User = loggedInuser;
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string requestPath)
+        {
+            ViewBag.RequestPath = requestPath ?? "/user";
+            return Index();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!IsAuthentic(model.Username, model.Password))
+                return View();
+
+            // create claims
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, model.Username)
+            };
+
+            // create identity
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "cookie");
+
+            // create principal
+            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+            // sign-in
+            await HttpContext.SignInAsync(
+                    scheme: "Fitness247UserAuthentication",
+                    principal: principal,
+                    properties: new AuthenticationProperties
+                    {
+                        IsPersistent = model.RememberMe, // for 'remember me' feature
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(30)
+                    });
+
+            return Redirect(model.RequestPath ?? "/user");
+        }
+        public async Task<IActionResult> Logout(string requestPath)
+        {
+            await HttpContext.SignOutAsync(
+                    scheme: "Fitness247UserAuthentication");
+
+            return Redirect("/");
+        }
+
+        private bool IsAuthentic(string username, string password)
+        {
+            int count = db_context.Users.Count(x => x._UserRole < UserRoles.Instructor && (x.Email == username || x.UserName == username) && x.Password == password);
+            if (count == 1)
+                return true;
+            return false;
+        }
+
+        public PartialViewResult BookingsCalendar()
+        {
+            List<Class> classes = db_context.Classes.AsNoTracking().ToList<Class>();
+            ViewBag.Classes = classes;
+            return PartialView();
+        }
+
+        public PartialViewResult MyReservations()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult MyPurchases()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public JsonResult GetClasses()
+        {
+            var searchParam = HttpContext.Request.Form["search[value]"];
+            var start = HttpContext.Request.Form["start"][0];
+            var length = HttpContext.Request.Form["length"][0];
+            var totalCount = db_context.Classes.Count();
+            var filterCount = db_context.Classes.Count();
+            List<dynamic> filteredClasses = db_context.Classes.Include(c => c.ClassType).ThenInclude(c => c.Location).Include(r => r.ClassSchedule).AsNoTracking().Skip(int.Parse(start)).Take(int.Parse(length)).ToList<dynamic>();
+
+            if (searchParam[0].Trim() != "")
+            {
+                filteredClasses = db_context.Classes.Include(c => c.ClassType).ThenInclude(c => c.Location).Include(r => r.ClassSchedule).AsNoTracking().Where(x => x.ClassType.ClassName.Contains(searchParam[0]) || x.ClassSchedule.ScheduleName.Contains(searchParam[0])).Skip(int.Parse(start)).Take(int.Parse(length)).ToList<dynamic>();
+                filterCount = filteredClasses.Count();
+            }
+
+            dynamic data = new
+            {
+                draw = HttpContext.Request.Form["draw"],
+                recordsTotal = totalCount,
+                recordsFiltered = filterCount,
+                data = filteredClasses
+            };
+            return Json(data);
+        }
+    }
+}
--- GroupProject/DB_Context.cs
+++ GroupProject/DB_Context.cs
@@ -14,6 +14,10 @@ namespace GroupProject
         public virtual DbSet<Instructor> Instructors { get; set; }
         public virtual DbSet<Location> Locations { get; set; }
         public virtual DbSet<ClassPackage> ClassPackages { get; set; }
+        public virtual DbSet<ClassType> ClassTypes { get; set; }
+        public virtual DbSet<RoomLayout> RoomLayouts { get; set; }
+        public virtual DbSet<ClassSchedule> ClassSchedules { get; set; }
+        public virtual DbSet<Class> Classes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
@@ -56,6 +60,106 @@ namespace GroupProject
                 .ValueGeneratedOnAdd();
                 modelBuilder.Entity<ClassPackage>().ToTable("class_packages")
                .Property(e => e.ExpirationDate).HasColumnType("Date");
+            });
+
+            modelBuilder.Entity<ClassType>(entity=>
+            {
+                modelBuilder.Entity<ClassType>().ToTable("class_types")
+                .Property(c => c.ClassTypeID)
+                .ValueGeneratedOnAdd();
+
+                modelBuilder.Entity<ClassType>().ToTable("class_types")
+                .HasOne(p => p.Location)
+                .WithMany(b => b.ClassTypes)
+                .HasForeignKey(p => p.LocationID)
+                .HasConstraintName("FK_ClassType_Location_LocationID");
+
+                modelBuilder.Entity<ClassType>().ToTable("class_types")
+                .HasOne(p => p.Instructor)
+                .WithMany(b => b.ClassTypes)
+                .HasForeignKey(p => p.InstructorID)
+                .HasConstraintName("FK_ClassType_User_UserID");
+
+            });
+
+            modelBuilder.Entity<RoomLayout>(entity =>
+            {
+                modelBuilder.Entity<RoomLayout>().ToTable("room_layouts")
+                .Property(c => c.RoomLayoutID)
+                .ValueGeneratedOnAdd();
+
+                modelBuilder.Entity<RoomLayout>().ToTable("room_layouts")
+                .HasOne(p => p.Location)
+                .WithMany(b => b.RoomLayouts)
+                .HasForeignKey(p => p.LocationID)
+                .HasConstraintName("FK_RoomLayout_Location_LocationID");
+            });
+
+            modelBuilder.Entity<ClassSchedule>(entity => {
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .Property(c => c.ClassScheduleID)
+                .ValueGeneratedOnAdd();
+
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .Property(e => e.StartDate).HasColumnType("Date");
+
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .Property(e => e.EndDate).HasColumnType("Date");
+
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .Property(e => e.StartTime).HasColumnType("Time");
+
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .Property(e => e.EndTime).HasColumnType("Time");
+
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .HasOne(c => c.ClassType)
+                .WithMany(c=>c.ClassSchedules)
+                .HasForeignKey(c => c.ClassTypeID)
+                .HasConstraintName("FK_ClassSchedule_ClassType_ClassTypeID");
+
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .HasOne(c => c.RoomLayout)
+                .WithMany(c => c.ClassSchedules)
+                .HasForeignKey(c => c.RoomLayoutID).IsRequired(false);
+                //.HasConstraintName("FK_ClassSchedule_RoomLayout_RoomLayoutID");
+
+                modelBuilder.Entity<ClassSchedule>().ToTable("class_schedules")
+                .Ignore(p => p.SelectedDays);
+            });
+
+            modelBuilder.Entity<Class>(entity => {
+                modelBuilder.Entity<Class>().ToTable("classes")
+               .Property(c => c.ClassID)
+               .ValueGeneratedOnAdd();
+
+
+                modelBuilder.Entity<Class>().ToTable("classes")
+                .Property(e => e.StartDate).HasColumnType("Date");
+
+                modelBuilder.Entity<Class>().ToTable("classes")
+                .Property(e => e.EndDate).HasColumnType("Date");
+
+                modelBuilder.Entity<Class>().ToTable("classes")
+                .Property(e => e.StartTime).HasColumnType("Time");
+
+                modelBuilder.Entity<Class>().ToTable("classes")
+                .Property(e => e.EndTime).HasColumnType("Time");
+
+                modelBuilder.Entity<Class>().ToTable("classes")
+                .HasOne(c => c.ClassType)
+                .WithMany(c => c.Classes)
+                .HasForeignKey(c => c.ClassTypeID);
+
+                modelBuilder.Entity<Class>().ToTable("classes")
+                .HasOne(c => c.ClassSchedule)
+                .WithMany(c => c.Classes)
+                .HasForeignKey(c => c.ClassScheduleID);
+
+                modelBuilder.Entity<Class>().ToTable("classes")
+                .HasOne(c => c.SubstituteInstructor)
+                .WithMany(c => c.Classes)
+                .HasForeignKey(c => c.InstructorID).IsRequired(false);
             });
         }
     }
